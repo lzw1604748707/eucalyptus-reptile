4e7d7cb583fcc60792f3093fbc7c72f63f5dae94
刘志伟 <lzw@spacetech.com.cn>
2019-09-26 16:01:10 +0800

    eslint初始化调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: [
+    'plugin:vue/essential',
+    // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier/@typescript-eslint',
+    // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    // 此行必须在最后
+    'plugin:prettier/recommended'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-    // 'prettier/prettier': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'off'
     // '@vue/prettier': 'off'
   },
   parserOptions: {
